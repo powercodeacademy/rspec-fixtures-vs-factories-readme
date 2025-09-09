5b95c39c79bfb6a8ab6f02c9345924ea1f2bae7e
Jen Kelly <jen.kelly@powerhrg.com>
2025-09-09 09:53:53 -0400

    Add and test volunteer role trait

--- spec/factories/volunteers.rb
+++ spec/factories/volunteers.rb
@@ -1,6 +1,13 @@
 FactoryBot.define do
   factory :volunteer do
     sequence(:name) { |n| "Volunteer#{n}" }
+
+    role { "member" }
+
+    trait :manager do
+      role { "manager" }
+    end
+
     association :organization
   end
 end
